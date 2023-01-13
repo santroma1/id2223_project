f934363440fbb1ee39401d6afaef8001d4f77d96
Santiago Roman <santroma1@gmail.com>
2023-01-13 11:33:02 -0600

    model training, use notebook not script

---
+++ model_training.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import hopsworks
+import os 
+# from dotenv import load_dotenv
+import requests
+import json
+import datetime
+import urllib.request
+import codecs
+import csv
+import sys
+from functions import *
+
+import pandas as pd
+
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import f1_score
+
+import warnings
+warnings.filterwarnings("ignore")
+
+
+
+if __name__ == "__main__":
+    # load_dotenv()
+    # AIR_QUALITY_API_KEY = os.getenv("AIR_QUALITY_API_KEY")
+    # WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+
+
+    # AIR_QUALITY_API_KEY = os.environ.get("AIR_QUALITY_API_KEY")
+    # WEATHER_API_KEY = os.environ.get("WEATHER_API_KEY")
+    # HOPWSORKS_API_KEY = os.environ.get("HOPSWORKS_API_KEY")
+
+    pass
+
