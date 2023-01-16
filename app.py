2714c7d25ada06d9dd2aa4a7280af1ad26c59ab3
Santiago Roman <santroma1@gmail.com>
2023-01-16 16:13:28 +0100

    quick app update

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ air_fg = air_fg.show(20).sort_values('date', ignore_index=True)
 prediction = air_fg.iloc[-1]['pred_aqi']
 date = air_fg.iloc[-1]['date']
 date_str = datetime.date.fromtimestamp(date//1000).strftime("%Y-%m-%d")
+date_tomorrow = air_fg.iloc[-1]["tomorrow_date"]
+date_tomorrow_str = datetime.date.fromtimestamp(date_tomorrow//1000).strftime("%Y-%m-%d")
+
 
 
 with gr.Blocks() as demo:
@@ -32,7 +35,8 @@ with gr.Blocks() as demo:
         
     with gr.Row():
       with gr.Column():
-          gr.Label(f"Predicted AQI for {date_str} in Madrid is {prediction}") 
+          gr.Label(f"Today is {date_str}") 
+          gr.Label(f"The Predicted AQI for tomorrow {date_tomorrow_str} in Madrid is {prediction}") 
           input_img = gr.Image("images/" + img_string, elem_id="predicted-img")            
 
 demo.launch()
